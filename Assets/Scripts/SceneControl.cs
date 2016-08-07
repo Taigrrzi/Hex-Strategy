f00b50256f5bf9fa3569034290f77d0c6b513e19
James Doran <jysdoran@gmail.com>
2016-08-07 12:42:49 +0800

    Added a menu screen

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Scene scene = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/Initial");
+    }
+}
